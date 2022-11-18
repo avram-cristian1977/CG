3bd6005d955bd53e2c052dca52a9131df50d095d
avram-cristian1977 <asct1977ro@gmail.com>
2022-11-18 08:46:36 +0200

    basic student model and UI fixes

---
+++ src/app/models/student.model.ts
@@ -0,0 +1,10 @@
+export interface IStudent {
+  id: string;
+  firstName: string;
+  lastName: string;
+  phone: string;
+  email: string;
+  birthDay: Date;
+  enrolmentDay: Date;
+  parents: string[];
+}
